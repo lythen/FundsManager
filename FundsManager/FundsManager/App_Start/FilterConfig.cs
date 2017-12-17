a4fbe4e32f4ab7315ef762d14073acf8ac174ff2
Lythen-7 <lyrenlian@163.com>
2017-12-17 15:44:37 +0800

    新建项目

---
+++ FundsManager/FundsManager/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using FundsManager.Infrastructure;
+
+namespace FundsManager.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
